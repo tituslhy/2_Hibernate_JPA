cee803e3b89b67d2dabb5eaec15d63685d628f6c
Titus Lim <tituslhy@gmail.com>
2025-11-28 15:27:27 +0800

    hello world cli

--- src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -1,7 +1,9 @@
 package com.luv2code.cruddemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CruddemoApplication {
@@ -9,5 +11,18 @@ public class CruddemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CruddemoApplication.class, args);
 	}
+
+    /**
+     * Used for command line applications using Spring boot
+     * Will be executed after the Spring Beans have been loaded
+     * @param args
+     * @return
+     */
+    @Bean
+    public CommandLineRunner commandLineRunner(String[] args){
+        return runner -> {
+            System.out.println("Hello world");
+        };
+    }
 
 }
